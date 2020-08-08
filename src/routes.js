f57f915aa0c17dfe43eec605a663646b51dfa4ae
Greyson Rampazo <greysonf@gmail.com>
2020-08-08 19:51:21 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const routes = express.Router()
+
+const SessionController = require('./controllers/SessionController')
+const OngController = require('./controllers/OngController')
+const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
+
+routes.post('/sessions', SessionController.store)
+
+routes.get('/ongs', OngController.index)
+routes.post('/ongs', OngController.store)
+
+routes.get('/incidents', IncidentController.index)
+routes.post('/incidents', IncidentController.store)
+routes.delete('/incidents/:id', IncidentController.destroy)
+
+routes.get('/profile', ProfileController.index)
+
+
+module.exports = routes
